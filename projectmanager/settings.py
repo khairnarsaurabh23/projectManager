93404e9dac1fbbc5b6ab0b469cfe595fda82e4bc
Khairnar Saurabh Ravindra <srkhairnar23@gmail.com>
2021-12-23 20:09:53 +0530

    django project to host code

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u9kk=0@kki)5k41rcz_vk1-&!_uz-4^n&v7%sd#d-+k=!=%aog'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
